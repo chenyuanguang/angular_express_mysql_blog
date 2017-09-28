e54e219b93f2e3c5e9a0a70b4d78d78a6995dd4b
guang <837990335@qq.com>
2017-09-28 23:37:45 +0800

    0928

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    api: "http://192.168.1.1:8088/",
+    databaseConfig: {
+        host: "localhost",
+        user: "root",
+        password: "1q2w3e",
+        database: "blog"
+    }
+}
